ddc395867b4ac778a58bb650ae4ec406f66648c4
j-asif <jalal.asif97@gmail.com>
2020-11-30 21:44:35 -0400

    Cleaned up the data, added user input for analysis

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 # This program takes a string of text and analyzes the frequency of each letter and plots a chart of it. This is
 # entirely for fun!
 
-#This is the text we will be analyzing
-text = "“The biggest component in any human, filling 61 percent of available space, is oxygen. It may seem a " \
-       "touch counterintuitive that we are almost two-thirds composed of an odorless gas. The reason we are not " \
-       "light and bouncy like a balloon is that the oxygen is mostly bound up with hydrogen (which accounts for " \
-       "another 10 percent of you) to make water—and water, as you will know if you have ever tried to move a " \
-       "wading pool or just walked around in really wet clothes, is surprisingly heavy. It is a little ironic " \
-       "that two of the lightest things in nature, oxygen and hydrogen, when combined form one of the heaviest, " \
-       "but that’s nature for you. Oxygen and hydrogen are also two of the cheaper elements within you. All " \
-       "your oxygen will set you back just $14 and your hydrogen a little over $26 (assuming you are about the " \
-       "size of Benedict Cumberbatch). Your nitrogen (2.6 percent of you) is a better value still at just forty " \
-       "cents for a body’s worth. But after that it gets pretty expensive."
+#This is the text we will be analyzing; as per user input
+print("Please type in the text you would like to have analyzed. ")
+text = input()
 
 #Dictionary to hold the letters and their respective frequency
 letter_count = {}
@@ -27,17 +19,26 @@ for letter in text:
 import matplotlib.pyplot as plt
 
 #print
-print(letter_count)
+#print(letter_count)
 #Separate into two separate items from dictionary and couple them into two variables
-a,b = zip(*letter_count.items())
+#a,b = zip(*letter_count.items())
 
 #plot data and show results
-plt.bar(a,b)
-plt.show()
+#plt.bar(a,b)
+#plt.show()
 
 ##A cleaner version of the code is below
 
 #Initialize an empty dictionary
 letterCountClean = {}
+
+for k,v in letter_count.items():
+       if k.isalpha():
+              letterCountClean[k] = v
+
+a,b = zip(*letterCountClean.items())
+
+plt.bar(a,b)
+plt.show()
 
 
